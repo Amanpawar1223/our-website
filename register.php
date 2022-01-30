6084ecdd23c8f94d1ac6eeec64caba1ff969c552
Aman <amanpawar476@gmail.com>
2022-01-30 13:24:52 +0530

    first commit

---
+++ register.php
@@ -0,0 +1,43 @@
+<?php
+require_once "config.php";
+
+$email;
+$email_err ;
+
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    // Check if email is empty
+    if(empty(trim($_POST["email"]))){
+        $email_err = "email cannot be blank";
+    }
+    else{
+        $sql = "SELECT id FROM users WHERE email = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        if($stmt)
+        {
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+
+            // Set the value of param email
+            $param_email = trim($_POST['email']);
+
+            // Try to execute this statement
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1)
+                {
+                    $email_err = "This email is already taken"; 
+                }
+                else{
+                    $email = trim($_POST['email']);
+                }
+            }
+            else{
+                echo "Something went wrong";
+            }
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+
+?>
+
